5e6d2012370dff157e18b62f840a39b5c8f36a2a
Gangadhara Sayi inti <gangadhara.inti@standard.com>
2023-02-24 10:47:09 -0800

    new

--- deser.java
+++ deser.java
@@ -1,1 +1,27 @@
+import java.io.*;
+public class Deser {
 
+   public static void main(String [] args) {
+      Emp e = null;
+      try {
+         FileInputStream fileIn = new FileInputStream("tmp/employee.ser");
+         ObjectInputStream in = new ObjectInputStream(fileIn);
+         e = (Emp) in.readObject();
+         in.close();
+         fileIn.close();
+      } catch (IOException i) {
+         i.printStackTrace();
+         return;
+      } catch (ClassNotFoundException c) {
+         System.out.println("Employee class not found");
+         c.printStackTrace();
+         return;
+      }
+      
+      System.out.println("Deserialized Employee...");
+      System.out.println("Name: " + e.name);
+      System.out.println("Address: " + e.address);
+      System.out.println("SSN: " + e.SSN);
+      System.out.println("Number: " + e.number);
+   }
+}
--- ser.java
+++ ser.java
@@ -1,1 +1,22 @@
+import java.io.*;
+public class Ser {
 
+   public static void main(String [] args) {
+      Emp e = new Emp();
+      e.name = "Reyan Ali";
+      e.address = "Phokka Kuan, Ambehta Peer";
+      e.SSN = 11122333;
+      e.number = 101;
+      
+      try {
+         FileOutputStream fileOut = new FileOutputStream("tmp/employee.ser");
+         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+         out.writeObject(e);
+         out.close();
+         fileOut.close();
+         System.out.printf("Serialized data is saved in tmp/employee.ser");
+      } catch (IOException i) {
+         i.printStackTrace();
+      }
+   }
+}
